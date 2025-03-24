ed759f0d41d27b3a9322fae9e8595bb8b29ab8df
Inhu Jo <joinhu@quescompany.com>
2025-03-24 14:20:13 +0900

    chore: vite-plugin-dts 플러그인 추가
    - d.ts 파일 생성 플러그인

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
+      name: '@ques-utils',
       entry: 'src/index.ts',
       formats: ['es', 'cjs'],
       fileName: (format) => `index.${format}.js`,
     },
     minify: 'esbuild',
   },
+  plugins: [dts()],
 });
