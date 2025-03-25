b49c684a3e130972065bc2834ab7f60d7a0ceb60
Inhu Jo <joinhu@quescompany.com>
2025-03-25 10:59:29 +0900

    feat: function → 화살표 함수로 변경

--- src/validation/email.ts
+++ src/validation/email.ts
@@ -1,6 +1,4 @@
 const regExp =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
 
-export function isEmail(value: string): boolean {
-  return regExp.test(value);
-}
+export const isEmail = (value: string): boolean => regExp.test(value);
